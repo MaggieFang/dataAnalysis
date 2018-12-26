7991714399598e08b72298c7481b1fba03d477ca
Maggie Fang <menogenfong@gmail.com>
2018-12-25 20:18:52 -0500

    more detail

--- pinnerbuy.R
+++ pinnerbuy.R
@@ -1,31 +1,22 @@
-library(Information)
 library(caret)
 library(mlbench)
-library(randomForest)
-library(rgl)
-
 
 folder = "/Users/xfang7/Documents/RCode/dataAnalysis/"
 user =  read.csv(paste(folder,"sessions.csv", sep=''))
 df =  read.csv(paste(folder,"transactions.csv", sep=''))
 # merge two data file
 df = merge(df,user,by.x="session_id",by.y="session_id")
-# sort by userId
-rank = order(df$user_id)
-df = df[rank,]
+# sort by userId and then date
+df = df[with(df,order(df$user_id,df$session_dt)),]
+
 df$conversion = as.numeric(df$conversion)
 
 # get some information of data
 table(df$conversion)
-summary(df)
-plot(df$conversion~df$num_search)
+hist(df$num_impressions,main = "Histogram of num_impression",xlab = "num_impression")
+hist(df$num_search,main = "Histogram of num_search",xlab = "num_search")
+hist(df$avg_relevance,main = "Histogram of avg_relevance",xlab = "avg_relevance")
 
-hist(df$num_impressions)
-hist(df$num_search)
-hist(df$avg_relevance)
-
-sapply(subset(df,select = c("conversion","num_impressions","avg_relevance","num_search")), sd)
-xtabs(~conversion + num_impressions, data = df)
 x = xtabs(~conversion + num_search, data = df)
 plot(x[1,], type="o", col="blue",axes=FALSE, ann=FALSE)
 axis(1, at=1:8, lab=c("0", "1", "2", "3","4", "5","6","7"))
@@ -36,54 +27,37 @@ title(xlab="num_search", col.lab=rgb(0,0.5,0))
 title(ylab="total", col.lab=rgb(0,0.5,0))
 legend("topright",c("conversion=0","conversion=1"), cex=0.8, col=c("blue","red"), pch=21:22, lty=1:2)
 
+#相关性
+correlationMatrix = cor(df[,4:6])
+print(correlationMatrix)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.75)
+print(highlyCorrelated)
 
-# divide into two file buy/not buy
+# divide into two set  buy/not buy
 buy = df[df$conversion == 1,]
-notBuy = df[df$conversion == 0,]
-lu=length(unique(buy$user_id))
-print(lu==nrow(buy)) # buy的数量跟userId都是一样的7500，说明买的用户有买就是1次，ps，总的user有15000
+buy_user_num=length(unique(buy$user_id))
+not_buy_user_num= length(unique(df$user_id)) - buy_user_num
+print(user_number_buy==nrow(buy)) # TRUE,user purchase 1 times, after that, even push again, he/she not buy any more
 
-# count = 0
-# factor_train = factor(df$user_id)
-# level = levels(factor_train)
-# for(i in 1:length(level)){
-#   user_id = level[i]
-#   idx = which(df$user_id == user_id)
-#   data = df[idx,]
-#   data = data[order(data$session_dt),]
-#   for(j in 1:nrow(data)){
-#    # print(paste("here,,",j,data[j,]$user_id,data[j,]$session_dt, sep=' '))
-#     if(data[j,]$conversion == 1 && j != nrow(data)){
-#       k = nrow(data)
-#       while(k >= j+1){
-#         if(data[k,]$score == FALSE){
-#           print(paste("delete,",k,data[k,]$user_id,data[k,]$session_dt, sep=' '))
-#           count = count+1
-#           df = subset(df,df$session_id != data[k,]$session_id)
-#         }
-#         k= k-1
-#       }
-#       break
-#     }
-#   }
-# }
+'%ni%' <- Negate('%in%')  # define 'not in' func
+buy_detail = df[df$user_id %in% buy$user_id,]
+not_buy_detail = df[df$user_id %ni% buy$user_id,]
+buy_mean_sesion =  nrow(buy_detail)/buy_user_num
+not_buy_mean_sesion =  nrow(not_buy_detail)/not_buy_user_num
+#conclusion: user purchase make more search
+buy_mean_search = sum(buy_detail$num_search)/buy_user_num
+not_buy_mean_search = sum(not_buy_detail$num_search)/not_buy_user_num
 
+buy_mean_relevance = sum(buy_detail$avg_relevance)/buy_user_num
+not_buy_mean_relevance =sum(not_buy_detail$avg_relevance)/not_buy_user_num
+
+hist(buy_detail$avg_relevance,main = "Histogram of avg_relevance for buy",xlab = "avg_relevance")
+hist(not_buy_detail$avg_relevance,main = "Histogram of avg_relevance for not-buy",xlab = "avg_relevance")
 
 # train data,test data,
 df.train = df[df$train == TRUE,]
 df.test = df[df$train == FALSE,]
 df.fit = df[df$score == TRUE,]
-
-# 观察train中true的user是否出现在test，结果只有一个
-trainTrue = df.train[df.train$conversion == 1,]
-for(i in nrow(df.test)){
-  df.test[i,]$user_id %in% trainTrue$user_id
-  print(paste(i,",traintrue,",df.test[i,]$conversion, df.test[i,]$user_id,sep=''))
-}
-
-#相关性
-correlationMatrix = cor(df[,4:6])
-print(correlationMatrix)
 
 mylogit <- glm(conversion ~  avg_relevance + num_search , data = df.train, family = "binomial")
 summary(mylogit)
@@ -115,6 +89,14 @@ table(df$conversion,df$pred)
 
 
 #######################以下是之前尝试#######################
+
+# 观察train中true的user是否出现在test，结果只有一个
+trainTrue = df.train[df.train$conversion == 1,]
+for(i in nrow(df.test)){
+  df.test[i,]$user_id %in% trainTrue$user_id
+  print(paste(i,",traintrue,",df.test[i,]$conversion, df.test[i,]$user_id,sep=''))
+}
+
 
 #删除purchase后还推的数据
 factor_train = factor(df.train$user_id)
